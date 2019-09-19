b144bac6bf7d947fb851fe757dc357923273abc0
Hong Xu <hong@topbug.net>
2019-09-19 16:14:35 -0700

    Move the CUDA implementation of trunc to ATen.
    Fix #24650
    
    ghstack-source-id: 147c27986a8f7435b1a0babf0efb4b560b8ee480
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25423

--- test/test_torch.py
+++ test/test_torch.py
@@ -11821,7 +11821,7 @@ class TestTorchDeviceType(TestCase):
             ("tanh", doubles, True, True, 'cpu'),
             ("tanh", doubles, False, True, 'cuda'),
             ("trunc", doubles, True, True, 'cpu'),
-            ("trunc", doubles, False, True, 'cuda')
+            ("trunc", doubles, True, True, 'cuda')
         ]
 
         for (fn, inputs, has_input_output_mem_overlap_check,
